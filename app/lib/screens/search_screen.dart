75918af2231a6bfce80b3f7a44b574c20cac7e48
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-18 09:58:06 +0300

    Update search_screen.dart

--- app/lib/screens/search_screen.dart
+++ app/lib/screens/search_screen.dart
@@ -5,46 +5,82 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final categories = [
+      "Trending",
+      "Music",
+      "Gaming",
+      "Sports",
+      "Travel",
+      "Fashion",
+      "Food",
+    ];
+
     return Scaffold(
       backgroundColor: const Color(0xFF08080F),
       appBar: AppBar(
         backgroundColor: const Color(0xFF08080F),
         elevation: 0,
-        centerTitle: true,
         title: Container(
           height: 45,
           decoration: BoxDecoration(
             color: const Color(0xFF1A1A24),
-            borderRadius: BorderRadius.circular(15),
+            borderRadius: BorderRadius.circular(14),
           ),
           child: const TextField(
             style: TextStyle(color: Colors.white),
             decoration: InputDecoration(
+              border: InputBorder.none,
+              prefixIcon: Icon(Icons.search,color: Colors.grey),
               hintText: "Search ONYX",
               hintStyle: TextStyle(color: Colors.grey),
-              prefixIcon: Icon(Icons.search, color: Colors.grey),
-              border: InputBorder.none,
             ),
           ),
         ),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(6),
-        itemCount: 30,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          crossAxisSpacing: 4,
-          mainAxisSpacing: 4,
-        ),
-        itemBuilder: (context, index) {
-          return ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.network(
-              "https://picsum.photos/400/400?random=$index",
-              fit: BoxFit.cover,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 45,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context,index){
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal:6),
+                  child: Chip(
+                    backgroundColor: const Color(0xFF1A1A24),
+                    label: Text(
+                      categories[index],
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(6),
+              itemCount: 30,
+              gridDelegate:
+                  const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                crossAxisSpacing: 4,
+                mainAxisSpacing: 4,
+              ),
+              itemBuilder: (context,index){
+                return ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.network(
+                    "https://picsum.photos/500?random=$index",
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }

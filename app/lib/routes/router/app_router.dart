bb66df3227ec5e19b84c05a6d3e666d2f4c8adb7
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-21 08:56:20 +0300

    Create app_router.dart

---
+++ app/lib/routes/router/app_router.dart
@@ -0,0 +1,11 @@
+/// ============================================================
+/// ONYX
+/// App Router
+/// ------------------------------------------------------------
+/// Central router configuration for the ONYX application.
+/// This file will manage all application navigation.
+/// ============================================================
+
+class AppRouter {
+  // TODO(ONYX): Configure GoRouter after creating all screens.
+}

3d3b7f38b5146302ac709684899cf7e30d9cad80
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-21 12:23:09 +0300

    Create app_service.dart

---
+++ app/lib/core/services/app_service.dart
@@ -0,0 +1,16 @@
+/// ============================================================
+/// ONYX APP
+/// Global Services
+/// ============================================================
+
+class AppService {
+  AppService._();
+
+  static Future<void> initialize() async {
+    // سيتم تهيئة جميع الخدمات هنا لاحقًا
+    // Firebase
+    // Notifications
+    // Storage
+    // Analytics
+  }
+}

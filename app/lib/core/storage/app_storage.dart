4a79e2ac593dee3469dc8a74bd20559b0febe547
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-21 12:26:31 +0300

    Create app_storage.dart

---
+++ app/lib/core/storage/app_storage.dart
@@ -0,0 +1,18 @@
+/// ============================================================
+/// ONYX APP
+/// Storage Service
+/// ============================================================
+/// Responsible for local storage.
+/// Future implementations:
+/// - SharedPreferences
+/// - Secure Storage
+/// - Hive / Isar
+/// ============================================================
+
+class AppStorage {
+  AppStorage._();
+
+  static Future<void> initialize() async {
+    // TODO: Initialize local storage.
+  }
+}

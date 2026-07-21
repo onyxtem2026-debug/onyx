508b62f07ee129743736fd9bd4ec11dfa3514072
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-21 09:15:15 +0300

    Create app_router.dart

---
+++ app/lib/routes/router/app/lib/routes/router/app_router.dart
@@ -0,0 +1,15 @@
+/// ============================================================
+/// ONYX
+/// App Router
+/// ============================================================
+/// This file is responsible for application routing.
+/// Navigation logic will be added in later stages.
+/// ============================================================
+
+class AppRouter {
+  const AppRouter._();
+
+  static void initialize() {
+    // TODO: Configure GoRouter later.
+  }
+}

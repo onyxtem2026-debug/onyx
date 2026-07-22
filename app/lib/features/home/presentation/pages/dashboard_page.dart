612dd0b3d8c550e535d8797be9002e183e5046dd
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-22 13:10:02 +0300

    Create dashboard_page.dart

---
+++ app/lib/features/home/presentation/pages/dashboard_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text(
+          'Dashboard Page',
+          style: TextStyle(
+            fontSize: 24,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}

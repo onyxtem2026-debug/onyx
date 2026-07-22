7faac10ecc8b952a58e4d0ec270819cc54e201b7
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-22 13:08:31 +0300

    Create home_screen.dart

---
+++ app/lib/features/home/presentation/pages/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomePage();
+  }
+}

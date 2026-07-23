1946bc57a071d1a768f4bf0721c7631aec16f4d0
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-23 11:39:52 +0300

    Create chat_list_page.dart

---
+++ features/chat/chat_list_page.dart
@@ -0,0 +1,58 @@
+import 'package0:flutter/material.dart';
+
+/// Onyx Chat List Page - Enterprise Architecture Standard
+class ChatListPage extends StatefulWidget {
+  const ChatListPage({super.key});
+
+  @override
+  State<ChatListPage> createState() => _ChatListPageState();
+}
+
+class _ChatListPageState extends State<ChatListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Chats',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.edit_note_outlined, size: 28),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              radius: 26,
+              backgroundColor: Colors.grey[800],
+              child: Text(
+                'U${index + 1}',
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            title: Text(
+              'User Name ${index + 1}',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            subtitle: const Text(
+              'Hey! Check out the new ONYX feature...',
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            trailing: Text(
+              '12:${30 + index} PM',
+              style: TextStyle(color: Colors.grey[500], fontSize: 12),
+            ),
+            onTap: () {},
+          );
+        },
+      ),
+    );
+  }
+}

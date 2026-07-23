429d3431b0fa9783f6b4a5a5d90eb05b2bebfac8
onyxtem2026-debug <onyxtem2026@gmail.com>
2026-07-23 20:21:40 +0300

    Update main.dart

--- apps/mobile/lib/main.dart
+++ apps/mobile/lib/main.dart
@@ -1,32 +1,7 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const OnyxApp());
-}
-
-class OnyxApp extends StatelessWidget {
-  const OnyxApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'ONYX',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'ONYX',
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
